e6ef3cd36764895fb49c59611cfc68424664b4a9
Stephen Solis <ssolis8619@gmail.com>
2021-03-31 15:18:36 -0500

    wire up notes.html to server.js file

--- server.js
+++ server.js
@@ -7,14 +7,26 @@ const PORT = 8080;
 
 // Create a function for handling the requests and responses coming into our server
 const handleRequest = (req, res) => {
-  // Here we use the fs package to read our index.html file
-  fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
-    if (err) throw err;
-    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-    // an html file.
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
+  // Capture the url the request is made to
+  const path = req.url;
+
+  // When we visit different urls, read and respond with different files
+  switch (path) {
+    case '/notes':
+      return fs.readFile(`${__dirname}/public/notes.html`, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      });
+
+    // default to rendering index.html, if none of above cases are hit
+    default:
+      return fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      });
+  }
 };
 
 // Create our server
