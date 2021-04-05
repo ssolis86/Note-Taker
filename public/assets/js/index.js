3e5e75de702793a76cf8e91fd0ebcb69f38c1dfd
Stephen Solis <ssolis8619@gmail.com>
2021-04-05 18:42:38 -0500

    get and post requests on server.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -81,7 +81,7 @@ const handleNoteDelete = (e) => {
   e.stopPropagation();
 
   const note = e.target;
-  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  // const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
 
   if (activeNote.id === noteId) {
     activeNote = {};
@@ -96,7 +96,7 @@ const handleNoteDelete = (e) => {
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
-  activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+  // activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   renderActiveNote();
 };
 
--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-// Dependencies
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const dataBase = require('./db/db')
+// uniqid offers the unique id functionality
+const uniqid = require('uniqid');
+console.log(uniqid.process());
 
-// Set our port to 8080
-const PORT = 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// Create a function for handling the requests and responses coming into our server
-const handleRequest = (req, res) => {
-  // Capture the url the request is made to
-  const path = req.url;
-
-  // When we visit different urls, read and respond with different files
-  switch (path) {
-    case '/notes':
-      return fs.readFile(`${__dirname}/public/notes.html`, (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      });
-
-    // default to rendering index.html, if none of above cases are hit
-    default:
-      return fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      });
-  }
-};
-
-// Create our server
-const server = http.createServer(handleRequest);
-
-// Starts our server
-server.listen(PORT, () => {
-  console.log(`Server is listening on PORT: ${PORT}`);
+app.get('/notes', (req, res) => 
+{
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+app.post('/api/notes', (req, res) =>
+{
+  notesData.push(req.body);
+  res.json(dataBase);
+})
+//
+app.post('api/notes', (req, res) => 
+{
+  req.body.id = uniqid.process();
+  console.log(uniqid.process());
+})
+
+app.get('*', (req,res) => 
+{
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
