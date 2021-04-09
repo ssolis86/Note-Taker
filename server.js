e0940b78f546e800ce53af1471191781d07ab946
Stephen Solis <ssolis8619@gmail.com>
2021-04-08 21:51:54 -0500

    debug post function, finalize delete function

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-const dataBase = require('./db/db')
-// uniqid offers the unique id functionality
+const dataBase = require('./db/db.json');
 const uniqid = require('uniqid');
-console.log(uniqid.process());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+//Add notes to list
+app.post('/api/notes', (req, res) => 
+{
+  req.body.id = `${uniqid.process()}`;
+  dataBase.push(req.body);
+  res.json(dataBase);
+});
+// Delete notes from list
+app.delete('/api/notes/:id', (req, res) =>
+{ 
+  dataBase.forEach(function(note, index, object)
+  {
+    if (note.id === req.params.id) {
+      object.splice(index, 1);
+    }
+  })
+  res.json(dataBase);
+});
+
+app.get('/api/notes', (req, res) =>
+{
+  res.json(dataBase);
+});
 
 app.get('/notes', (req, res) => 
 {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
-app.post('/api/notes', (req, res) =>
-{
-  notesData.push(req.body);
-  res.json(dataBase);
-})
-//
-app.post('api/notes', (req, res) => 
-{
-  req.body.id = uniqid.process();
-  console.log(uniqid.process());
-})
 
 app.get('*', (req,res) => 
 {
